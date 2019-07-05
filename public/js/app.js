9d629ba2e66cbf9e3f5883ceb4fd8ba4d8a676a6
uriiz <uri.izhaki@gmail.com>
2019-07-05 09:35:31 +0300

    xcxc

--- public/js/app.js
+++ public/js/app.js
@@ -72134,8 +72134,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\projects\Air-delivery\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\projects\Air-delivery\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\project\Air-delivery\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\project\Air-delivery\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
